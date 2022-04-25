9f4fb2517a56d5213a9f5aa7aadde4cd172edf79
florence <florencecodestoo@gmail.com>
2022-04-24 21:45:03 -0400

    remove adding text into html, import util func, update how name is rendered

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+import displayArrayItems from '../util/displayArrayItems.js'
+
 const POKEMON_URL = 'https://pokeapi.co/api/v2/pokemon'
 
 /* const for specified elements for readability */
 const select = document.querySelector('.pokemonList');
 const imgContainer = document.querySelector('.pokemon-img');
+const pokemonNameLabel = document.querySelector('#name');
 const typeContainer = document.querySelector('#type');
 const experienceContainer = document.querySelector('#experience');
 const heightContainer = document.querySelector('#height');
@@ -37,32 +40,26 @@ const getPokemonInfo = (url) => {
 
       /* storing data in vars from pokemon data */
       let svgUrl = data.sprites.other.dream_world.front_default;
+      let pokemonName = data.name;
       let elementType = data.types.map(obj => obj.type.name);
       let baseExp = data.base_experience;
       let heightNum = Number(data.height * 0.1).toFixed(1);
       let weightNum = Number(data.weight * 0.1).toFixed(1);
       let knownAbilitiesArray = data.abilities.filter(obj => obj.is_hidden === false).map(obj => obj.ability.name);
+      console.log(knownAbilitiesArray)
       let hiddenAbilitiesArray = data.abilities.filter(obj => obj.is_hidden === true).map(obj => obj.ability.name);
 
-      //TO-DO: put below util func in a util folder?
       //TO-DO: account for dashes between words
-      /* util func to display array items */
-      function displayArrayItems (array) {
-        let itemsString = ``; //TO-DO: figure out how to put a comma into every entry except for ending entry
-        for (let item of array) {
-           itemsString += `${item[0].toUpperCase()}` + `${item.slice(1)} `
-        }
-        return itemsString;
-      }
   
       /* put info on the page */
       imgContainer.src = svgUrl;
-      typeContainer.textContent = `Type: ${displayArrayItems(elementType)}`;
-      experienceContainer.textContent = `Base Exp: ${baseExp}`
-      heightContainer.textContent = `Height: ${heightNum} metres`
-      weightContainer.textContent = `Weight: ${weightNum} kilograms`
-      knownAbilitiesContainer.textContent = `Abilities: ${displayArrayItems(knownAbilitiesArray)}`
-      hiddenAbilitiesContainer.textContent = hiddenAbilitiesArray.length > 0 ? `Hidden Abilities: ${displayArrayItems(hiddenAbilitiesArray)}` : `Hidden Abilities: None`
+      pokemonNameLabel.textContent = `${pokemonName[0].toUpperCase()}` + `${pokemonName.slice(1)}`
+      typeContainer.textContent = elementType > 1 ? displayArrayItems(elementType) : elementType
+      experienceContainer.textContent = baseExp
+      heightContainer.textContent = `${heightNum} metres`
+      weightContainer.textContent = `${weightNum} kilograms`
+      knownAbilitiesContainer.textContent = displayArrayItems(knownAbilitiesArray)
+      hiddenAbilitiesContainer.textContent = hiddenAbilitiesArray.length > 0 ? displayArrayItems(hiddenAbilitiesArray) : 'none'
     });
 }
 
@@ -70,8 +67,7 @@ const getPokemonInfo = (url) => {
     let selectedPokemonUrl = `${POKEMON_URL}/${event.target.value.toLowerCase()}`;
     getPokemonInfo(selectedPokemonUrl);
 
-    const pokemonNameLabel = document.querySelector('#name');
-    pokemonNameLabel.textContent = `Name: ${event.target.value[0].toUpperCase()}` + `${event.target.value.slice(1)}`;
+    // pokemonNameLabel.textContent = `Name: ${event.target.value[0].toUpperCase()}` + `${event.target.value.slice(1)}`;
   })
 
 
