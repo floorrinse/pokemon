06d46b7aac0da36ba508b232d3e32f90e88a28c2
florence <florencecodestoo@gmail.com>
2022-05-10 01:10:14 -0400

    feat: include more than og 150 pokemon, beta version

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 import punctuateArray from '../util/punctuateArray.js'
 import convertToTitleCase from '../util/convertToTitleCase.js';
-import pokemonNames from '../util/pokemonNames.js';
 
 /* const for specified elements for readability */
 const POKEMON_URL = 'https://pokeapi.co/api/v2/pokemon'
-// const select = document.querySelector('.pokemonList');
 const searchInput = document.querySelector('.searchInput');
 const searchSubmit = document.querySelector('.searchSubmit');
 const pokemonNameTitle = document.querySelector('.pokemonTitle');
@@ -18,6 +16,16 @@ const weightContainer = document.querySelector('#weight');
 const knownAbilitiesContainer = document.querySelector('#knownAbilities');
 const hiddenAbilitiesContainer = document.querySelector('#hiddenAbilities');
 
+let allPokemonNames;
+
+fetch(`${POKEMON_URL}/?limit=100000&offset=0`)
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    allPokemonNames = data.results.map(pokemonObj => pokemonObj.name);
+})
+
 const getPokemonInfo = (url) => {
   fetch(url)
     .then(res => {
@@ -26,9 +34,11 @@ const getPokemonInfo = (url) => {
     .then(data => {
       /* storing data in vars from pokemon data */
       let svgUrl = data.sprites.other.dream_world.front_default;
+      let altSvgUrl = data.sprites.other["official-artwork"].front_default;
+      let pokeApipokemonName = data.name;
+      console.log(pokeApipokemonName);
       let pokemonName = data.name;
       let elementType = data.types.map(obj => obj.type.name);
-      console.log(elementType);
       let hp = data.stats.filter(item => item.stat.name === 'hp')[0].base_stat;
       let attack = data.stats.filter(item => item.stat.name === 'attack')[0].base_stat;
       let defense = data.stats.filter(item => item.stat.name === 'defense')[0].base_stat;
@@ -36,9 +46,9 @@ const getPokemonInfo = (url) => {
       let weightNum = Number(data.weight * 0.1).toFixed(1);
       let knownAbilitiesArray = data.abilities.filter(obj => obj.is_hidden === false).map(obj => obj.ability.name);
       let hiddenAbilitiesArray = data.abilities.filter(obj => obj.is_hidden === true).map(obj => obj.ability.name);
-  
+
       /* put info on the page */
-      imgContainer.src = svgUrl;
+      imgContainer.src = data.sprites.other.dream_world.front_default ? svgUrl : altSvgUrl;
       imgContainer.alt = pokemonName;
       pokemonNameTitle.textContent = convertToTitleCase(pokemonName);
       typeContainer.textContent = punctuateArray(elementType)
@@ -55,39 +65,10 @@ const getPokemonInfo = (url) => {
   searchSubmit.addEventListener('click', event => {
     event.preventDefault();
     let pokemonUrl = `${POKEMON_URL}/${searchInput.value.toLowerCase()}`;
-    if (pokemonNames.includes(searchInput.value.toLowerCase())) {
+    if (allPokemonNames.includes(searchInput.value.toLowerCase())) {
       getPokemonInfo(pokemonUrl);
     }
     else {
       alert('Please make sure your spelling is correct.')
     }
 })
-
-
-
-
-// function displayTypes (types) {
-//   for (let i = 0; i < types.length; i++) {
-//     typesContainer.textContent += convertToTitleCase(types[i]);
-//      console.log(types[i])
-//   }
-//   select.addEventListener('change', () => {
-//     while (typesContainer.lastChild) {
-//       typesContainer.removeChild(typesContainer.lastChild);
-//     }
-//   })
-// }
-
-// function createSpanElementType (types) {
-//     for (let i=0;i<types.length;i++) {
-//      const span = document.createElement('span');
-//      span.className = "elementType"
-//      span.textContent = convertToTitleCase(types[i]);
-//      typesContainer.appendChild(span);
-//    }
-//     select.addEventListener('change', () => {
-//       while (typesContainer.lastChild) {
-//         typesContainer.removeChild(typesContainer.lastChild);
-//       }
-//     })
-//   }
